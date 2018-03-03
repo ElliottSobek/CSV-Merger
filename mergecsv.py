7d32ec5c416d6bd3c3828138b5d380ee606617c3
Elliott <esobek@bbex.com>
2018-03-02 19:52:25 -0700

    Add header license & execution license

--- mergecsv.py
+++ mergecsv.py
@@ -1,3 +1,17 @@
+#     Merge CSV; Merge multiple CSV files together
+#     equivalent will be generated.
+#     Copyright (C) 2018  Elliott Sobek
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
 import os
 import sys
 import csv
@@ -9,6 +23,10 @@ def main(argc=len(sys.argv), argv=sys.argv):
     if argc < 2:
         print("Usage: python3 " + basename(argv[0]) + " <filename.csv ...> <outfile>")
         return
+
+    print("Merge CSV Copyright (C) 2018  Elliott Sobek\n"
+          "This program comes with ABSOLUTELY NO WARRANTY.\n"
+          "This is free software, and you are welcome to redistribute it under certain conditions.\n")
 
     for i in range(1, argc - 1):
         if not argv[i].endswith('.csv'):
