ec0b8aff0c5e6cc582e03cdba410c43632e9b412
Elliott <esobek@bbex.com>
2018-03-07 20:25:43 -0700

    Refactor to use OptionParser

--- mergecsv.py
+++ mergecsv.py
@@ -14,73 +14,58 @@
 #     GNU General Public License for more details.
 
 import sys
-import getopt
 import csv
 
 from os.path import basename, getsize, exists
+from optparse import OptionParser
 
 
-def main(argc=len(sys.argv), argv=sys.argv):
-    base_name = basename(argv[0])
+def main():
+    parser = OptionParser(usage="Usage: python3 %prog [options] <filename.csv ...> <outfile>", version="%prog 0.1")
+    parser.add_option("-v", "", action="store_true", dest="vertical_flag", help="Merge files vertically")
+    parser.add_option("-d", "", action="store_true", dest="diagonal_flag", help="Merge files diagonally")
 
-    if 4 < argc or 2 > argc:
-        print("Usage: python3 " + base_name + " [-hvd] <filename.csv ...> <outfile>")
+    options, args = parser.parse_args()
+
+    if len(args) is not 3:
+        print("Usage: python3 " + basename(sys.argv[0]) + " [-hvd] <filename.csv ...> <outfile>")
         sys.exit(1)
 
-    try:
-        opts, args = getopt.getopt(argv[1:], "hvd")
-    except getopt.GetoptError as e:
-        print(e.msg)
-        sys.exit(1)
+    in_files = args[:-1]
+    out_file = args[-1]
 
-    vertical_flag = False
-    diagonal_flag = False
-
-    for opt, args in opts:
-        if opt == "-h":
-            print("Usage: python3 " + base_name + " [-hvd] <filename.csv ...> <outfile>\n\n"
-                  "Default merge axis is horizontal (x axis)\n"
-                  "\t-h\tHelp\n\n"
-                  "\t-v\tMerge files vertically\n\n"
-                  "\t-d\tMerge files diagonally\n\n")
-            sys.exit()
-        elif opt == "-v":
-            vertical_flag = True
-            print("WIP")
-        elif opt == "-d":
-            diagonal_flag = True
-            print("WIP")
-
-    if vertical_flag and diagonal_flag:
+    if options.vertical_flag and options.diagonal_flag:
         print("Error: more than one merge axis flags set")
         sys.exit(1)
 
-    for i in range(1, argc):
-        if not exists(argv[i]):
-            print("Error: " + argv[i] + "does not exist")
+    for i in range(len(in_files)):
+        if not exists(in_files[i]):
+            print("Error: " + in_files[i] + " does not exist")
+            sys.exit(1)
+        if not in_files[i].endswith('.csv'):
+            print("Error: " + in_files[i] + " must be comma separated value (csv) format")
             sys.exit(1)
 
-    for i in range(1, argc):
-        if not argv[i].endswith('.csv'):
-            print("Error: Passed in file(s) must be comma separated value (csv) format")
-            sys.exit(1)
-
-    if getsize(argv[1]) and getsize(argv[2]):
-        output = open(argv[3], 'w', newline='')
-        writer = csv.writer(output)
-        writer.writerow(['Error: no data was provided'])
-        output.close()
-        return
-    elif getsize(argv[1]):
-        # copyfile(argv[2], argv[3])
-        return
-    elif getsize(argv[2]):
-        # copyfile(argv[1], argv[3])
-        return
+    if not out_file.endswith('.csv'):
+        print("Error: " + out_file + " must be comma separated value (csv) format")
+        sys.exit(1)
 
     print("Merge CSV Copyright (C) 2018  Elliott Sobek\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
           "This is free software, and you are welcome to redistribute it under certain conditions.\n")
+
+    # if getsize(argv[arg_index]) and getsize(argv[arg_index + 1]):
+    #     output = open(argv[3], 'w', encoding='utf-8', newline='')
+    #     writer = csv.writer(output)
+    #     writer.writerow(['Error: no data was provided'])
+    #     output.close()
+    #     return
+    # elif getsize(argv[arg_index]):
+    #     # copyfile(argv[2], argv[3])
+    #     return
+    # elif getsize(argv[arg_index + 1]):
+    #     # copyfile(argv[1], argv[3])
+    #     return
 
     filename = sys.argv[-1]
 
