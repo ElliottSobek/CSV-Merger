d33565f263e2881d93bd29aa3a9653bf497156d2
Elliott <esobek@bbex.com>
2018-03-30 13:53:34 -0600

    Improve imports & format

--- mergecsv.py
+++ mergecsv.py
@@ -13,12 +13,11 @@
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 
-import os
-import sys
-import csv
-
+from os import name, access, R_OK
 from os.path import basename, getsize, exists, dirname, splitext
+from sys import exit, argv
 from optparse import OptionParser
+from csv import writer
 
 
 def gen_outfile(filepath, extension):
@@ -27,7 +26,7 @@ def gen_outfile(filepath, extension):
     base_outfile = splitext(out_base)[0]
 
     if out_dir:
-        if os.name == "nt":
+        if name == "nt":
             return out_dir + '\\' + base_outfile + extension
         return out_dir + '/' + base_outfile + extension
     return out_dir + base_outfile + extension
@@ -35,6 +34,7 @@ def gen_outfile(filepath, extension):
 
 def main():
     parser = OptionParser(usage="Usage: python3 %prog [options] <filename.csv ...> <outfile>", version="%prog 0.1")
+
     parser.add_option("-v", action="store_true", dest="vertical_flag", help="Merge files vertically")
     parser.add_option("-d", action="store_true", dest="diagonal_flag", help="Merge files diagonally")
     parser.add_option("-f", action="store_true", dest="force_flag", help="Force merge if an input file is empty")
@@ -42,40 +42,44 @@ def main():
     options, args = parser.parse_args()
 
     if len(args) is not 3:
-        print("Usage: python3 " + basename(sys.argv[0]) + " [-hvdf] <filename.csv ...> <outfile>")
-        sys.exit(1)
+        print("Usage: python3 " + basename(argv[0]) + " [-hvdf] <filename.csv ...> <outfile>")
+        exit(1)
 
     in_files = args[:-1]
     out_file_name = args[-1]
 
     if options.vertical_flag and options.diagonal_flag:
         print("Error: more than one merge axis flags set")
-        sys.exit(1)
+        exit(1)
+
+    extension = ".csv"
 
     for file in in_files:
         if not exists(file):
             print("Error: " + file + " does not exist")
-            sys.exit(1)
-        if not file.endswith('.csv'):
+            exit(1)
+        if not file.endswith(extension):
             print("Error: " + file + " must be comma separated value (csv) format")
-            sys.exit(1)
+            exit(1)
         if not options.force_flag and not getsize(file):
             print("Error: " + file + " is empty")
-            sys.exit(1)
-    extension = ".csv"
+            exit(1)
 
     if not out_file_name.endswith(extension):
         out_file_name = gen_outfile(out_file_name, extension)
+    if access(out_file_name, R_OK):
+        print("Error: " + out_file_name + " is readonly")
+        exit(1)
 
     print("Merge CSV Copyright (C) 2018  Elliott Sobek\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
           "This is free software, and you are welcome to redistribute it under certain conditions.")
 
-    outfile = open(out_file_name, 'w', encoding='utf-8', newline='')
-    out_writer = csv.writer(outfile)
+    outfile = open(out_file_name, 'w', encoding="utf-8", newline='')
+    out_writer = writer(outfile)
 
     for in_filename in in_files:
-        in_file = open(in_filename, 'r', encoding='utf-8', newline='')
+        in_file = open(in_filename, 'r', encoding="utf-8", newline='')
 
         in_file.close()
     outfile.close()
